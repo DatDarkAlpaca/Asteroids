24e705dad0a8c0564d78e204ec20abc443b69ddb
Alpaca <70183390+DatDarkAlpaca@users.noreply.github.com>
2021-11-08 13:39:05 -0400

    Added settings and workshop scene placeholders.

---
+++ Asteroids/src/scenes/SceneWorkshop.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "SceneWorkshop.h"
+
+void ast::SceneWorkshop::Initialize()
+{
+}
+
+void ast::SceneWorkshop::PollEvents(const sf::Event& event)
+{
+}
+
+void ast::SceneWorkshop::Update(float dt)
+{
+}
+
+void ast::SceneWorkshop::Render(sf::RenderWindow& window)
+{
+}
+
+void ast::SceneWorkshop::Cleanup()
+{
+}
---
+++ Asteroids/src/scenes/SceneWorkshop.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Scene.h"
+#include "gui/GUI.h"
+
+namespace ast
+{
+
+	class SceneWorkshop : public Scene
+	{
+	public:
+		virtual void Initialize() override;
+
+		virtual void PollEvents(const sf::Event& event) override;
+
+		virtual void Update(float dt) override;
+
+		virtual void Render(sf::RenderWindow& window) override;
+
+		virtual void Cleanup() override;
+
+	private:
+		gui::Container m_MainContainer;
+	};
+
+}
+
